a002018f8546531947cda387a6b04c48421afb2f
yaraaos <yara144ss@gmail.com>
2025-01-17 19:00:49 +0200

    generic task 2

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,10 @@ type AllType = {
   weight: number
 }
 
-function compare<T extends AllType, U extends AllType> (top: Pick<T, 'name' | 'color'>, bottom: Pick<U, 'position' | 'weight'>): AllType {
+function compare<T extends Pick<AllType, 'name' | 'color'>, U extends Pick<AllType, 'position' | 'weight'>>(
+  top: T,
+  bottom: U
+): AllType {
   return {
     name: top.name,
     color: top.color,
